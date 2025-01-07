90010913e1835e71a51e69c367af91573908be12
LuisCartucho <luipac01@easv365.dk>
2025-01-07 13:28:47 +0100

    add - edit buttons

--- src/main/java/easv/intgrpmovie/HelloApplication.java
+++ src/main/java/easv/intgrpmovie/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("My Movies");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/easv/intgrpmovie/HelloController.java
+++ src/main/java/easv/intgrpmovie/HelloController.java
@@ -1,16 +1,21 @@
 package easv.intgrpmovie;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     private ListView<String> category;
@@ -18,26 +23,41 @@ public class HelloController {
     @FXML
     private ListView<String> movieCategoriesList;
 
-    public void initialize() {
-        // Adaugăm categoriile de filme în ListView
-        movieCategoriesList.getItems().addAll(
-                "Action", "Animation", "Comedy", "Crime", "Drama", "Film-noir", "Horror", "Thriller", "War", "Western"
-        );
-        category.getItems().addAll("Movie");
-    }
-
     public void btnaddMovie(ActionEvent actionEvent) {
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("add-removeMovies.fxml"));
 
             Stage stage = new Stage();
             Scene scene = new Scene(fxmlLoader.load());
-            stage.setTitle("add-remove-movie");
+            stage.setTitle("Add Movie");
             stage.setScene(scene);
             stage.show();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void btnEditMovie(ActionEvent actionEvent) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("add-removeMovies.fxml"));
+
+            Stage stage = new Stage();
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Edit Movie");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+               // Adaugăm categoriile de filme în ListView
+        movieCategoriesList.getItems().addAll(
+                "Action", "Animation", "Comedy", "Crime", "Drama", "Film-noir", "Horror", "Thriller", "War", "Western"
+        );
+        category.getItems().addAll("Movie");
     }
 }
 
