8b5b8090f05abb583382497bedd81800eeb49987
Thien Phuoc Lam <phuoclamthien@gmail.com>
2025-01-07 14:20:50 +0100

    pop-up window for add-edit movies set up constructor for movies class

---
+++ src/main/java/easv/intgrpmovie/be/Movies.java
@@ -0,0 +1,58 @@
+package easv.intgrpmovie.be;
+
+public class Movies {
+    private int id;
+    private String name;
+    private int rating;
+    private String filelink;
+    private String lastView;
+
+
+    public Movies(int id, String name, int rating, String filelink, String lastView) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.filelink = filelink;
+        this.lastView = lastView;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getFilelink() {
+        return filelink;
+    }
+
+    public void setFilelink(String filelink) {
+        this.filelink = filelink;
+    }
+
+    public String getLastView() {
+        return lastView;
+    }
+
+    public void setLastView(String lastView) {
+        this.lastView = lastView;
+    }
+}
